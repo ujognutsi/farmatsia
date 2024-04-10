33e55d89c7e90969f36b2bacb32020b72c4598b6
ksjrnf <georgarshakyan@yandex.ru>
2024-04-10 16:18:07 +0300

    commit by 10.04 16:18

--- app/models.py
+++ app/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class TagManager(models.Manager):
+    pass
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class QuestionManager(models.Manager):
+    def get_by_tag(self, Tag):
+        return self.filter(Tag in self.tags)
+    
+    # def get_hot(self):
+    #     return self.filter(self.id == question_id )
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.CharField(max_length=65535)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+class AnswerManager(models.Manager):
+    pass
+
+class Answer(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.CharField(max_length=65535)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class ProfileManager(models.Manager):
+    pass
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class QuestionLikeManager(models.Manager):
+    pass
+
+class QuestionLike(models.Model):
+    question_id = models.ManyToOneRel(Question, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    unique_together = [
+        ['question_id', 'user_id']
+    ]
+
+    def __str__(self):
+        return self.question_id
+
+class AnswerLikeManager(models.Manager):
+    pass
+
+class AnswerLike(models.Model):
+    answer_id = models.OneToOneField(Answer, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    unique_together = [
+        ['answer_id', 'user_id']
+    ]
+
+    def __str__(self):
+        return self.answer_id
+    
+
+# вывести вопросы сортировкой по убыванию суммы questionlike с question_id == id
+# SELECT * FROM question ORDER BY COUNT(SELECT * FROM questionlike WHERE questiion_id == id)
--- askme_malkhasyan/settings.py
+++ askme_malkhasyan/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,10 +78,17 @@ WSGI_APPLICATION = 'askme_malkhasyan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'yury',
+        'PASSWORD': 'YURY',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
+
 }
+
+
 
 
 # Password validation
