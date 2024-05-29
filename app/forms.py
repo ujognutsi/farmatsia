87370d6faaedd20476aaab36a718436d2e37d7d9
Arshak <georgarshakyan@yandex.ru>
2024-05-29 10:26:48 +0300

    some real changes

--- app/forms.py
+++ app/forms.py
@@ -1,27 +1,26 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from app.models import Profile, User
+from app.models import CustomUser
 from django.db import models
-from django.core.validators import validate_email
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-    # def clean(self):
-    #     super().clean()
-    #     if self.cleaned_data['username'] != self.cleaned_data['password']:
-    #         raise ValidationError('Login and password don\'t match!')
+    def clean(self):
+        super().clean()
+        if self.cleaned_data['username'] != self.cleaned_data['password']:
+            raise ValidationError('Login and password don\'t match!')
+            print('soemethi')
         
 class RegisterForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['login', 'email', 'password', 'confirm_password', 'avatar']
+        model = CustomUser
+        fields = ['login', 'password', 'avatar']
 
-    login = forms.CharField(min_length=5, max_length=20, required=True)
-    email = forms.EmailField(required=True, widget=forms.EmailInput)
-    password = forms.CharField(min_length=6, max_length=32, required=True, widget=forms.PasswordInput)
-    confirm_password = forms.CharField(min_length=6, max_length=32, required=True, widget=forms.PasswordInput)
+    login = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
     avatar = forms.ImageField()
     
     def clean(self):
@@ -35,40 +34,7 @@ class RegisterForm(forms.ModelForm):
 
     # commit = False - вызываем метод, но в базе не сохраняем
     def save(self, commit=True):
-        user = User.objects.create_user(username=self.cleaned_data['login'], password=self.cleaned_data['password'], email=self.cleaned_data['email'])
-        # user = super().save(commit=False)
-        profile = Profile(user=user)
-        if self.cleaned_data['avatar']:
-            setattr(profile, 'avatar', self.cleaned_data['avatar'])
-        profile.save()
-        # user.set_password(self.cleaned_data['password'])
-        # user.save()
-        return user
-    
-class EditProfileForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['login', 'email', 'avatar']
-
-    login = forms.CharField(min_length=5, max_length=20, required=True)
-    email = forms.EmailField(required=True, widget=forms.EmailInput)
-    avatar = forms.ImageField()
-
-    def save(self, commit=True):
-        user = self.instance
-        user.username = self.cleaned_data['login']
-        user.email = self.cleaned_data['email']
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
         user.save()
-        
-        profile = user.profile
-        profile.avatar = self.cleaned_data['avatar']
-        profile.save()
         return user
-    
-class QuestionForm(forms.ModelForm):
-    title = forms.CharField(min_length=10, max_length=255, required=True)
-    text = forms.CharField(min_length=100, max_length=65535, required=True)
-    # tagsInput = forms.CharField()
-
-class AnswerForm(forms.ModelForm):
-    text = forms.CharField(min_length=100, max_length=65535, required=True)
--- app/management/commands/filldb.py
+++ app/management/commands/filldb.py
@@ -8,10 +8,9 @@ class Command(BaseCommand):
         parser.add_argument("ratio", type=int) 
     
     def handle(self, *args, **options):
-        ratio = options["ratio"]
         users = []
         profiles = []
-        for i in range(ratio):
+        for i in range(options["ratio"]):
             users.append(User(id=i, username=f"User #{i}", password=f"Password{i}{i%1000}"))
             profiles.append(Profile(id=i, user=users[i]))
         
@@ -19,37 +18,37 @@ class Command(BaseCommand):
         Profile.objects.bulk_create(profiles)
         print("Users and profiles added")
         tags = []
-        for i in range(ratio):
+        for i in range(options["ratio"]):
             tags.append(Tag(id=i, name=f"Tag #{i}"))
         
         Tag.objects.bulk_create(tags)
         print("Tags added")
         questions = []
-        for i in range(ratio * 10):
+        for i in range(options["ratio"] * 10):
             questions.append(Question(id=i, title=f"Question #{i}", text=f"Question text #{i}"))
         
         Question.objects.bulk_create(questions)
 
-        for i in range(ratio * 10):
+        for i in range(options["ratio"] * 10):
             questions[i].tags.add(*random.sample(tags, random.randint(1, 5)))
 
         print("Questions added")
         answers = []
 
-        for i in range(ratio * 100):
+        for i in range(options["ratio"] * 100):
             answers.append(Answer(id=i, title=f"Answer #{i}", text=f"Answer #{i} text", 
-                                  question=Question.objects.get(id=random.randint(0, ratio * 10 - 1))))
+                                  question=Question.objects.get(id=random.randint(0, 99999))))
 
         Answer.objects.bulk_create(answers)
         print("Answers added")
         questionLikes = []
 
-        for i in range(ratio * 101):
-            randquestion = random.randint(0, ratio * 10 - 1)
-            randuser = random.randint(0, ratio - 1)
+        for i in range(options["ratio"] * 101):
+            randquestion = random.randint(0, 99999)
+            randuser = random.randint(0, 9999)
             while randquestion == randuser:
-                randquestion = random.randint(0, ratio * 10 - 1)
-                randuser = random.randint(0, ratio - 1)                
+                randquestion = random.randint(0, 99999)
+                randuser = random.randint(0, 9999)                
             questionLikes.append(
                 QuestionLike(
                     id=i, 
@@ -62,12 +61,12 @@ class Command(BaseCommand):
         print("QuestionLikes added")
         answerLikes = []
 
-        for i in range(ratio * 101):
-            randanswer = random.randint(0, ratio * 100 - 1)
-            randuser = random.randint(0, ratio - 1)
+        for i in range(options["ratio"] * 101):
+            randanswer = random.randint(0, 999999)
+            randuser = random.randint(0, 9999)
             while randanswer == randuser:
-                randanswer = random.randint(0, ratio * 100 - 1)
-                randuser = random.randint(0, ratio - 1)     
+                randanswer = random.randint(0, 999999)
+                randuser = random.randint(0, 9999)     
             answerLikes.append(
                 AnswerLike(
                     id=i,
